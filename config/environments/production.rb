7b5aa0a51891c95a698ff57f967d0af0cd01abab
Oscar <ochng6654@gmail.com>
2015-12-15 16:32:49 -0800

    Latest Revision

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'oscar-api.herokuapp.com'}
-
+  config.action_mailer.perform_deliveries = true
 end
