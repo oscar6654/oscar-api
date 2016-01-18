50a96d0dd8fe3de277bcfc48ddea2250b7b5902b
Oscar <ochng6654@gmail.com>
2016-01-17 16:57:47 -0800

    fix stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,14 +44,14 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
   config.action_mailer.raise_delivery_errors = true
 
-    ActionMailer::Base.smtp_settings = {
-        address:        'smtp.sendgrid.net',
-        port:           '587',
-        authentication: :plain,
-        user_name:      ENV['SENDGRID_USERNAME'],
-        password:       ENV['SENDGRID_PASSWORD'],
-        domain:         'heroku.com',
-        enable_starttls_auto: true
-    }
+    # ActionMailer::Base.smtp_settings = {
+    #     address:        'smtp.sendgrid.net',
+    #     port:           '587',
+    #     authentication: :plain,
+    #     user_name:      ENV['SENDGRID_USERNAME'],
+    #     password:       ENV['SENDGRID_PASSWORD'],
+    #     domain:         'heroku.com',
+    #     enable_starttls_auto: true
+    # }
 
 end
